3d2ba9cf6369d2b00c5e9fa28a03347edc419912
Rampathoti Ajithkumar <rampathotiajithkumar9330@gmail.com>
2026-07-15 12:24:47 +0530

    Add files via upload

---
+++ Week-4/1. spring-rest-handson/REST - Get country based on country code/src/main/java/com/cognizant/spring_learn/model/CountryList.java
@@ -0,0 +1,16 @@
+package com.cognizant.spring_learn.model;
+
+import java.util.List;
+
+public class CountryList {
+
+    private List<Country> country;
+
+    public List<Country> getCountry() {
+        return country;
+    }
+
+    public void setCountry(List<Country> country) {
+        this.country = country;
+    }
+}
