66c3f030a7a36967c4fa58482396e10c4f017546
Rampathoti Ajithkumar <rampathotiajithkumar9330@gmail.com>
2026-07-18 21:29:12 +0530

    Create App.js

---
+++ Week-5/React -1/3. ReactJS-HOL/src/App.js
@@ -0,0 +1,16 @@
+import { CalculateScore } from "./Components/CalculateScore";
+
+function App() {
+  return (
+    <div>
+      <CalculateScore
+        Name="AjithKumar"
+        School="SCOPE from VITAP"
+        total={945}
+        goal={1000}
+      />
+    </div>
+  );
+}
+
+export default App;
